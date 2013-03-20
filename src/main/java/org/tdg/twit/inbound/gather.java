fa7c9ba72db49df36155dee03d485d111f4093c4
bithack <markdazey@gmail.com>
2013-03-20 12:57:07 -0400

    started gather.java for reading the twitter feed

---
+++ src/main/java/org/tdg/twit/inbound/gather.java
@@ -0,0 +1,18 @@
+package org.tdg.twit.inbound;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.ConsoleAppender;
+
+public class Gather{
+    static Logger logger = Logger.getLogger(fpClassifier.class);
+    /**
+     *  @param args
+     */
+
+    public void start(){
+        logger.info("Starting to read Twitter feed");
+    }
+}
+
